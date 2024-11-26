ecffba511db24f979aeaa4dc6790f4010ee9f9cb
dayforjames <sergey.nikitin2013@gmail.com>
2024-11-27 00:16:31 +0300

    add basic render

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <LiteMath.h>
+
+#include "bitmap.hpp"
+
+using LiteMath::float3;
+using LiteMath::float2;
+
+float 
+sphere_sdf(float3 P, float3 P0, float R)
+{
+    return R - LiteMath::length(P - P0);
+}
+
+float tetrahedron_sdf(float3 v0, float3 v1, float3 v2, float3 v3, float3 p)
+{       
+    return LiteMath::max(
+        LiteMath::max(
+                LiteMath::dot(p, LiteMath::normalize(v0)),
+                LiteMath::dot(p, LiteMath::normalize(v1))
+        ),
+        LiteMath::max(
+                LiteMath::dot(p, LiteMath::normalize(v2)),
+                LiteMath::dot(p, LiteMath::normalize(v3))
+        )
+    );
+}
+
+float sierpinski_triangle_sdf(float3 v0, float3 v1, float3 v2, float3 v3, float3 p, float max_iter)
+{
+    for (int i = 0; i < max_iter; ++i)
+    {
+        p = LiteMath::abs(p);
+
+        if (p.x + p.y > 1.0)
+        {
+            p.x -= 1;
+            p.y -= 1;
+        }
+        
+        if (p.y + p.z > 1.0)
+        {
+            p.y -= 1;
+            p.z -= 1;
+        }
+
+        if (p.z + p.x > 1)
+        {
+            p.z -= 1;
+            p.x -= 1;
+        }
+
+        p *= 2.0;
+    }
+
+    return tetrahedron_sdf(v0, v1, v2, v3, p);
+}
+
+int
+main()
+{
+    const uint32_t WIDTH = 500, HEIGHT = 500;
+    const uint32_t MAX_ITER = 100;
+    const float EPS = 1e-2;
+    const float AP = (float)WIDTH / HEIGHT;
+
+    uint32_t data[WIDTH * HEIGHT] = {};
+
+    float3 v0 {10, 10, -40}, v1 {-10, -10, -40}, v2 {-10, 10, -60}, v3 {10, -10, -60};
+
+    for (float y = -1; y <= 1; y += 2.f / HEIGHT)
+    {
+        for (float x = -AP; x <= AP; x += 2.f * AP / WIDTH)
+        {
+            LiteMath::float3 P {x, y, -1};
+            LiteMath::float3 ray_origin {0.f, 0.f, 0.f};
+            LiteMath::float3 ray_dir = LiteMath::normalize(P - ray_origin);
+
+            float t = 0.001, dist = 1e5;
+            uint32_t iter = 1;
+
+            while (dist > EPS && iter < MAX_ITER)
+            {
+                float3 Point = ray_origin + t * ray_dir;
+                // dist = std::abs(sphere_sdf(Point, float3(0, 0, -100), 50));
+                dist = std::abs(tetrahedron_sdf(v0, v1, v2, v3, Point));
+                // std::cout << dist << std::endl;
+                if (dist < EPS)
+                {
+                    data[(int)((y + 1) * (float)HEIGHT / 2.f) * WIDTH + (int)((x + AP) * (float)WIDTH / 2.f)] = 0xffffff;
+                    break;
+                }
+                
+                t += dist;
+                iter++;
+            }
+        }
+    }
+
+    std::string name = "output.bmp";  
+    SaveBMP(name.c_str(), data, WIDTH, HEIGHT);
+
+    return 0;
+}
